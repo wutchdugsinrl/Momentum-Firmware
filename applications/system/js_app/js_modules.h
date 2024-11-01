090cc7a010e77f257e46307b70a842b0a4c3e777
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-11-01 13:28:20 +0000

    Update JS SDK vendor firmware

--- applications/system/js_app/js_modules.h
+++ applications/system/js_app/js_modules.h
@@ -9,7 +9,7 @@
 #define PLUGIN_APP_ID      "js"
 #define PLUGIN_API_VERSION 1
 
-#define JS_SDK_VENDOR_FIRMWARE "unleashed"
+#define JS_SDK_VENDOR_FIRMWARE "momentum"
 #define JS_SDK_VENDOR          "flipperdevices"
 #define JS_SDK_MAJOR           0
 #define JS_SDK_MINOR           1
